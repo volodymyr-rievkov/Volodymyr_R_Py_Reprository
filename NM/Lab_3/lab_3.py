e5d3c09e066834537e9b5e2573364c3900fc5733
volodymyr-rievkov <volodymyr.rievkov.shi.2023@lpnu.ua>
2024-10-19 16:23:54 +0300

    lab 4 TOI Finished

--- NM/Lab_3/lab_3.py
+++ NM/Lab_3/lab_3.py
@@ -9,12 +9,11 @@ def write_matrix_to_file(matrix, file_path):
         file.write('\n')
         np.savetxt(file, matrix, fmt='%f')
 
-def is_diagonaly_dominant(matrix):
+def is_diagonally_dominant(matrix):
     n = len(matrix)
     for i in range(n):
         row_sum = sum(abs(matrix[i][j]) for j in range(n) if j != i)
-        if abs(matrix[i][i]) <= row_sum:
-            print("Error: Matrix is not diagonaly dominant.")
+        if abs(matrix[i][i]) < row_sum:
             return False
     return True    
 
@@ -23,7 +22,6 @@ def is_symmetric(matrix):
     for i in range(n):
         for j in range(n):
             if matrix[i][j] != matrix[j][i]:
-                print("Error: Matrix is not symmetric.")
                 return False
     return True
 
@@ -32,44 +30,54 @@ def is_positively_defined(matrix):
     for i in range(1, n + 1):
         sub_matrix = matrix[:i, :i]
         if(np.linalg.det(sub_matrix) <= 0):
-            print("Error: Matrix is not positively defined.")
             return False
     return True
 
 def is_determinant(matrix):
     if (np.linalg.det(matrix) == 0):
-        print("Error: Matrix's determinant equals zero.")
         return False
     return True
 
-def make_diagonaly_dominant(matrix, b):
+def make_diagonally_dominant(matrix, b):
     n = len(matrix)
     for i in range(n):
-        if (abs(matrix[i][i]) <= sum(abs(matrix[i][j]) for j in range(n) if j != i)):
-            for j in range(i + 1, n):
-                if (abs(matrix[j][i]) > sum(abs(matrix[j][k]) for k in range(n) if k != i)):
-                    matrix[[i, j]] = matrix[[j, i]]
-                    b[i], b[j] = b[j], b[i]
-                    break
-            else:
-                for j in range(n):
-                    if ((j != i) and (matrix[j][i] != 0)):
-                        scale_factor = abs(matrix[i][i]) / abs(matrix[j][i])
-                        matrix[i] += scale_factor * matrix[j]
-                        b[i] += scale_factor * b[j]
-    if(is_diagonaly_dominant(matrix)):
-        return True
-    else:
-        return False
+        max_index = np.argmax(np.abs(matrix[i:n, i])) + i  
+        if (max_index != i): 
+            matrix[[i, max_index]] = matrix[[max_index, i]]  
+            b[i], b[max_index] = b[max_index], b[i] 
+    return is_diagonally_dominant(matrix) 
+
+def check_matrix(matrix, b):
+    check = True
+    if(not is_determinant(matrix)):
+       print("Error: System of equations can not be solved(det = 0).")
+       check = False
+    if(not is_diagonally_dominant(matrix)):
+        if(not make_diagonally_dominant(matrix, b)):
+            print("Error: The matrix is not diagonally dominant. The convergence of the Seidel method is not guaranteed.")
+            check = False
+    if(not is_symmetric(matrix)):
+        print("Warning: Matrix is not symmetric. The convergence of the Seidel method is not guaranteed.")
+    if(not is_positively_defined(matrix)):
+        print("Warning: Matrix is not positively defined. The convergence of the Seidel method is not guaranteed.")
+    return check
+
+def calculate_norm_c(matrix):
+    D = np.diag(np.diag(matrix))  
+    L = np.tril(matrix, -1)       
+    U = np.triu(matrix, 1)        
+    
+    D_L_inv = np.linalg.inv(D + L)
+    C = -D_L_inv.dot(U)
+    
+    norm_c = np.linalg.norm(C, np.inf)
+    return min(norm_c, 0.99)
 
 def seidel_method(matrix, b, tolerance):
     n = len(matrix)
     x = np.zeros(n)
-    if(not is_determinant(matrix) or not is_symmetric(matrix)or not is_positively_defined(matrix)):
-       return x
-    if(not is_diagonaly_dominant(matrix)):
-        if(not make_diagonaly_dominant(matrix, b)):
-            return x
+    q = calculate_norm_c(matrix)
+    iterations = 0
     while True:
         x_new = x.copy()
         for i in range(n):
@@ -77,12 +85,35 @@ def seidel_method(matrix, b, tolerance):
             sum2 = sum(matrix[i][j] * x[j] for j in range(i + 1, n))
             x_new[i] = (b[i] - sum1 - sum2) / matrix[i][i]
         error = max(abs(x_new[i] - x[i]) for i in range(n))
-        if (error < tolerance):
+        if (((1 - q) / q) * error < tolerance):
+            print("Number of iterations:", iterations)
             return x_new  
         x = x_new
+        iterations += 1
+
+def calculate_error(matrix, b, x):
+    Ax = np.dot(matrix, x)
+    error = np.linalg.norm(Ax - b)
+    return error
 
 file_path = "D:\\Studing\\Чисельні методи\\Лабораторна робота №3\\lab_3_3.txt"
 matrix, b = read_matrix_from_file(file_path)
-TOLERANCE = 1e-10
-x = seidel_method(matrix, b, TOLERANCE)
-write_matrix_to_file(x, file_path)
+
+print("Matrix:")
+print(matrix)
+print("Vector:")
+print(b)
+
+if(check_matrix(matrix, b)):
+
+    TOLERANCE = 1e-10
+    x = seidel_method(matrix, b, TOLERANCE)
+    error = calculate_error(matrix, b, x)
+
+    print("X:")
+    print(x)
+
+    print("Error: ")
+    print(error)
+
+    write_matrix_to_file(x, file_path)
--- TOI/Lab_4/lab_4.py
+++ TOI/Lab_4/lab_4.py
@@ -1,37 +1,40 @@
 from collections import Counter 
+from decimal import Decimal, getcontext
+
+getcontext().prec = 50
 
 def get_probs_dict(message):
     n = len(message)
     symbol_dict = Counter(message)
-    probs_dict = {symbol: count / n for symbol, count in symbol_dict.items()}
+    probs_dict = {symbol: Decimal(count) / Decimal(n) for symbol, count in symbol_dict.items()}
     return probs_dict
 
 def get_intervals_dict(probs_dict):
     intervals_dict = dict()
-    prob = 0.0
+    prob = Decimal(0.0)
     for s, p in probs_dict.items():
         low = prob
-        high = prob + p
+        high = prob + Decimal(p)
         intervals_dict[s] = (low, high)
         prob = high
     return intervals_dict
 
 def arithmetic_coding(message, intervals_dict):
-    low = 0.0
-    high = 1.0
+    low = Decimal(0.0)
+    high = Decimal(1.0)
     code_list = list()
     for s in message:
-       width = high - low
-       low = low + width * intervals_dict[s][0]
-       high = low + width * (intervals_dict[s][1] - intervals_dict[s][0])
-       code_list.append((s, low, high))
-    return low, code_list
+        width = high - low
+        high = low + width * intervals_dict[s][1]
+        low = low + width * intervals_dict[s][0]
+        code_list.append((s, low, high))
+    return Decimal((low + high) / 2), code_list
 
-def arithmetic_decoding(value, mess_len, intervals_dict):
+def arithmetic_decoding(value, intervals_dict, mess_len):
     decode_list = list()
     for _ in range(mess_len):
         for symbol, (low, high) in intervals_dict.items():
-            if (low <= value < high):
+            if low <= value < high:
                 decode_list.append((symbol, value, low, high))
                 value = (value - low) / (high - low)
                 break
@@ -80,31 +83,27 @@ def print_table_3(decode_list):
         print("|--------|--------------------|---------------|---------------|")
     print()   
 
-message = "Rievkov"
-#message_5 = ["X1", "X7", "X6", "X2", "X4"]
-#message_10 = ["X5", "X9", "X2", "X3", "X7", "X2", "X8", "X2", "X10", "X9"]
-
+message = "Rievkov Volodymyr Volodymyrovych"
 sequence = get_probs_dict(message)
-#equence_x = {"X1": 0.10, "X2": 0.09, "X3": 0.19, "X4": 0.10, "X5": 0.04, "X6": 0.13, "X7": 0.08, "X8": 0.10, "X9": 0.09, "X10": 0.10}
-
 i_sequence = get_intervals_dict(sequence)
-#_sequence_x = get_intervals_dict(sequence_x)
-
 print_table_1(i_sequence)
-#print_table_1(i_sequence_x)
-
 code_m, encoded_m = arithmetic_coding(message, i_sequence)
-#code_m_5, encoded_m_5 = arithmetic_coding(message_5, i_sequence_x)
-#code_m_10, encoded_m_10 = arithmetic_coding(message_10, i_sequence_x)
-
 print_table_2(encoded_m)
-#print_table_2(encoded_m_5)
-#print_table_2(encoded_m_10)
-
-decoded_m = arithmetic_decoding(code_m, len(message), i_sequence)
-#decoded_m_5 = arithmetic_decoding(code_m_5, len(message_5) i_sequence_x)
-#decoded_m_10 = arithmetic_decoding(code_m_10, len(message_10), i_sequence_x)
-
+decoded_m = arithmetic_decoding(code_m, i_sequence, len(message))
 print_table_3(decoded_m)
-#print_table_3(decoded_m_5)
-#print_table_3(decoded_m_10)
+
+sequence_x = {"X1": 0.10, "X2": 0.09, "X3": 0.19, "X4": 0.10, "X5": 0.04, "X6": 0.13, "X7": 0.08, "X8": 0.10, "X9": 0.09, "X10": 0.08}
+i_sequence_x = get_intervals_dict(sequence_x)
+print_table_1(i_sequence_x)
+
+message_5 = ["X1", "X7", "X6", "X2", "X4"]
+code_m_5, encoded_m_5 = arithmetic_coding(message_5, i_sequence_x)
+print_table_2(encoded_m_5)
+decoded_m_5 = arithmetic_decoding(code_m_5, i_sequence_x, len(message_5))
+print_table_3(decoded_m_5)
+
+message_10 = ["X5", "X9", "X2", "X3", "X7", "X2", "X8", "X2", "X10", "X9"]
+code_m_10, encoded_m_10 = arithmetic_coding(message_10, i_sequence_x)
+print_table_2(encoded_m_10)
+decoded_m_10 = arithmetic_decoding(code_m_10, i_sequence_x, len(message_10))
+print_table_3(decoded_m_10)
