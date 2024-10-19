07a125524157d4ef377f075ebe0beb078b282c44
volodymyr-rievkov <volodymyr.rievkov.shi.2023@lpnu.ua>
2024-10-19 21:38:17 +0300

    Added relative names to fields with foreign key, corrected fields names, added class Meta

--- AP/Lab_3/project/app/models.py
+++ AP/Lab_3/project/app/models.py
@@ -1,58 +1,82 @@
 from django.db import models
 
 class Delivery(models.Model):
-    delivery_id = models.AutoField(primary_key=True)  
+    id = models.AutoField(primary_key=True)  
     order = models.ForeignKey('Order', on_delete=models.CASCADE)  
     country = models.CharField(max_length=50)  
     city = models.CharField(max_length=50)  
     street = models.CharField(max_length=50)  
 
+    class Meta:
+        managed = False
+        db_table = 'delivery'
+        
     def __str__(self):
-        return f"Delivery id: {self.delivery_id} \nOrder: \n{self.order} \nCountry: {self.country} \nCity: {self.city} \nStreet: {self.street}"
+        return f"Delivery id: {self.id} \nOrder: \n{self.order} \nCountry: {self.country} \nCity: {self.city} \nStreet: {self.street}"
     
 class Discount(models.Model):
-    discount_id = models.AutoField(primary_key=True)  
-    product = models.ForeignKey('Product', on_delete=models.CASCADE)  
-    discount_value = models.SmallIntegerField()  
+    id = models.AutoField(primary_key=True)  
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='discounts')  
+    value = models.SmallIntegerField()  
 
+    class Meta:
+        managed = False
+        db_table = 'discount'
+        
     def __str__(self):
-        return f"Discount id: {self.discount_id} \nProduct: \n{self.product} \nValue: {self.discount_value}%"
+        return f"Discount id: {self.id} \nProduct: \n{self.product} \nValue: {self.value}%"
     
 class Order(models.Model):
-    order_id = models.AutoField(primary_key=True)  
+    id = models.AutoField(primary_key=True)  
     user = models.ForeignKey('User', on_delete=models.CASCADE) 
     product = models.ForeignKey('Product', on_delete=models.CASCADE)  
-    product_amount = models.SmallIntegerField()  
-    order_comment = models.TextField(blank=True, null=True)  
+    amount = models.SmallIntegerField()  
+    comment = models.TextField(blank=True, null=True)  
 
+    class Meta:
+        managed = False
+        db_table = 'оrder'
+        
     def __str__(self):
-        return f"Order id: {self.order_id} \nUser: \n{self.user} \nProduct: \n{self.product} \nAmount: {self.product_amount} \nComment: {self.order_comment}"
+        return f"Order id: {self.id} \nUser: \n{self.user} \nProduct: \n{self.product} \nAmount: {self.amount} \nComment: {self.comment}"
 
 class OrderHistory(models.Model):
     order = models.ForeignKey('Order', on_delete=models.CASCADE)  
     order_date_time = models.DateTimeField()
 
+    class Meta:
+        managed = False
+        db_table = 'order_history'
+        
     def __str__(self):
         return f"Order: \n{self.order} \nDate/Time: {self.order_date_time}"
 
 class User(models.Model):
-    user_id = models.AutoField(primary_key=True)  
-    user_first_name = models.CharField(max_length=50)  
-    user_last_name = models.CharField(max_length=50)  
-    user_phone_number = models.CharField(max_length=20)  
-    user_email = models.EmailField(max_length=50, unique=True)  
-    user_password = models.CharField(max_length=20)  
+    id = models.AutoField(primary_key=True)  
+    first_name = models.CharField(max_length=50)  
+    last_name = models.CharField(max_length=50)  
+    phone_number = models.CharField(max_length=20)  
+    email = models.EmailField(max_length=50, unique=True)  
+    password = models.CharField(max_length=20)  
 
+    class Meta:
+        managed = False
+        db_table = 'user'
+        
     def __str__(self):
-        return f"User id: {self.user_id} \nFirst Name: {self.user_first_name} \nLast name: {self.user_last_name} \nNumber: {self.user_phone_number} \nEmail: {self.user_email} \nPassword: {self.user_password}"
+        return f"User id: {self.id} \nFirst Name: {self.first_name} \nLast name: {self.last_name} \nNumber: {self.phone_number} \nEmail: {self.email} \nPassword: {self.password}"
 
 class Product(models.Model):
-    product_id = models.AutoField(primary_key=True)      
-    product_name = models.CharField(max_length=50)  
-    product_info = models.TextField(blank=True, null=True)  
-    product_price = models.DecimalField(max_digits=10, decimal_places=2)  
-    product_amount = models.IntegerField()  
-    discount = models.ForeignKey('Discount', blank=True, null=True, on_delete=models.SET_NULL)  
+    id = models.AutoField(primary_key=True)      
+    name = models.CharField(max_length=50)  
+    info = models.TextField(blank=True, null=True)  
+    price = models.DecimalField(max_digits=10, decimal_places=2)  
+    amount = models.IntegerField()  
+    discount = models.ForeignKey('Discount', blank=True, null=True, on_delete=models.SET_NULL, related_name='products')  
+
+    class Meta:
+        managed = False
+        db_table = 'product'
 
     def __str__(self):
-        return f"Product id: {self.product_id} \nName: {self.product_name} \nPrice: {self.product_price} \nAmount: {self.product_amount} \nInfo: {self.product_info} \nDiscount: {self.discount.discount_value if self.discount else '0.0'}%"
+        return f"Product id: {self.id} \nName: {self.name} \nPrice: {self.price} \nAmount: {self.amount} \nInfo: {self.info} \nDiscount: {self.discount.discount_value if self.discount else '0.0'}%"
