f26c432929f23f1fe56fb0ef2541a68a7106a599
volodymyr-rievkov <volodymyr.rievkov.shi.2023@lpnu.ua>
2024-10-23 16:54:24 +0300

    Lab_3

---
+++ AP/Lab_3/project_1/test_app.py
@@ -0,0 +1,22 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_1.settings")
+django.setup()
+
+from app.repository_factory import RepositoryFactory
+
+# users_repo = RepositoryFactory.get_repository("User")
+# users_repo.show_all()
+
+# discounts_repo = RepositoryFactory.get_repository("Discount")
+# discounts_repo.show_all()
+
+# products_repo = RepositoryFactory.get_repository("Product")
+# products_repo.show_all()
+
+# orders_repo = RepositoryFactory.get_repository("Order")
+# orders_repo.show_all()
+
+# delivery_repo = RepositoryFactory.get_repository("Delivery")
+# delivery_repo.show_all()
