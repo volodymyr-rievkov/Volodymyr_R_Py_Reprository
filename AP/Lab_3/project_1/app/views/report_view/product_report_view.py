4a6afc89c16c73f4db144d118b7206534220c99c
volodymyr-rievkov <volodymyr.rievkov.shi.2023@lpnu.ua>
2024-11-06 19:03:08 +0200

    _ commit

--- AP/Lab_3/project_1/app/views/report_view/product_report_view.py
+++ AP/Lab_3/project_1/app/views/report_view/product_report_view.py
@@ -7,6 +7,7 @@ from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 
+
 class ProductReportView(APIView, IReportView):
 
     permission_classes = [IsAuthenticated]
